adcd05d197498889026149e42dd0d075fbe061c3
Abhilash Vemula <vemulaabhisparp@gmail.com>
2020-02-07 12:37:54 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,11 +7,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to react</h1>
-      </header>
-        <p className="App-intro">
-          I was changed on the Feature branch.
+        <code>src/App.js</code> and save to reload.
+        <p>
+          I have changed the app text of App.js
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
